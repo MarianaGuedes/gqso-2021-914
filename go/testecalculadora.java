e3ba24163f373c7e13770e2a8fef975b0aaeaa45
MarianaGuedes <86361999+MarianaGuedes@users.noreply.github.com>
2021-12-01 22:17:55 -0300

    Delete go directory

--- go/testecalculadora.java
+++
@@ -1,9 +0,0 @@
-public class TesteCalculadora {
-    public static void main(String[] a){
-        Calculadora c = new Calculadora();
-        int valorUm = 5;
-        int valorDois = 5;
-        double valorTotal = c.somar(valorUm, valorDois);
-        assertEquals(valorTotal, 10);
-    }
-}
