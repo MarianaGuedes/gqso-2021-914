619a7720d7e4bc3011d7be081637c80fda296002
MarianaGuedes <86361999+MarianaGuedes@users.noreply.github.com>
2021-12-01 22:08:49 -0300

    Update testecalculadora.java

--- go/testecalculadora.java
+++ go/testecalculadora.java
@@ -4,11 +4,6 @@ public class TesteCalculadora {
         int valorUm = 5;
         int valorDois = 5;
         double valorTotal = c.somar(valorUm, valorDois);
-        if(valorTotal == 10){
-            System.out.println(" O valor está correto!");
-        }
-        else{
-            System.out.println("O valor está errado!");
-        }
+        assertEquals(valorTotal, 10);
     }
 }
